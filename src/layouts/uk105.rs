849dc5a36d65b21cd8093c8c783579303c165735
Jonathan Pallant (Ferrous Systems) <jonathan.pallant@ferrous-systems.com>
2023-02-04 20:18:35 +0000

    Fix UK Hash and Backslash mixup
    Closes #25

--- src/layouts/uk105.rs
+++ src/layouts/uk105.rs
@@ -53,14 +53,67 @@ impl KeyboardLayout for Uk105Key {
                     DecodedKey::Unicode('4')
                 }
             }
-            KeyCode::HashTilde => {
+            KeyCode::BackSlash => {
                 if modifiers.is_shifted() {
                     DecodedKey::Unicode('~')
                 } else {
                     DecodedKey::Unicode('#')
                 }
             }
+            KeyCode::HashTilde => {
+                if modifiers.is_shifted() {
+                    DecodedKey::Unicode('|')
+                } else {
+                    DecodedKey::Unicode('\\')
+                }
+            }
             e => <super::Us104Key as KeyboardLayout>::map_keycode(e, modifiers, handle_ctrl),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::{HandleControl, Keyboard, ScancodeSet2};
+
+    #[test]
+    fn test_hash() {
+        let mut k = Keyboard::<Uk105Key, ScancodeSet2>::new(HandleControl::MapLettersToUnicode);
+        // As seen on a UK 105 key Dell PS/2 keyboard when pressing `~#`
+        let ev = k.add_byte(0x5D).unwrap().unwrap();
+        let decoded_key = k.process_keyevent(ev);
+        assert_eq!(decoded_key, Some(DecodedKey::Unicode('#')));
+    }
+
+    #[test]
+    fn test_backslash() {
+        let mut k = Keyboard::<Uk105Key, ScancodeSet2>::new(HandleControl::MapLettersToUnicode);
+        // As seen on a UK 105 key Dell PS/2 keyboard when pressing `|\`
+        let ev = k.add_byte(0x61).unwrap().unwrap();
+        let decoded_key = k.process_keyevent(ev);
+        assert_eq!(decoded_key, Some(DecodedKey::Unicode('\\')));
+    }
+
+    #[test]
+    fn test_tilde() {
+        let mut k = Keyboard::<Uk105Key, ScancodeSet2>::new(HandleControl::MapLettersToUnicode);
+        // As seen on a UK 105 key Dell PS/2 keyboard when pressing Shift and `~#`
+        let ev = k.add_byte(0x12).unwrap().unwrap();
+        let _ = k.process_keyevent(ev);
+        let ev = k.add_byte(0x5D).unwrap().unwrap();
+        let decoded_key = k.process_keyevent(ev);
+        assert_eq!(decoded_key, Some(DecodedKey::Unicode('~')));
+    }
+
+    #[test]
+    fn test_pipe() {
+        let mut k = Keyboard::<Uk105Key, ScancodeSet2>::new(HandleControl::MapLettersToUnicode);
+        // As seen on a UK 105 key Dell PS/2 keyboard when pressing Shift and `|\`
+        let ev = k.add_byte(0x12).unwrap().unwrap();
+        let _ = k.process_keyevent(ev);
+        let ev = k.add_byte(0x61).unwrap().unwrap();
+        let decoded_key = k.process_keyevent(ev);
+        assert_eq!(decoded_key, Some(DecodedKey::Unicode('|')));
     }
 }
