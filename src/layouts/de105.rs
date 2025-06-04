f9f5d0c57503ad074bae673035eecbfe1a918041
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2025-06-04 21:53:26 +0100

    Update De105Key

--- src/layouts/de105.rs
+++ src/layouts/de105.rs
@@ -1,6 +1,8 @@
 //! German keyboard support
 
-use crate::{DecodedKey, HandleControl, KeyCode, KeyboardLayout, Modifiers, PhysicalKeyboard, QUO};
+use crate::{
+    DecodedKey, HandleControl, KeyCode, KeyboardLayout, Modifiers, PhysicalKeyboard, QUO, SLS,
+};
 
 /// A standard German 102-key (or 105-key including Windows keys) keyboard.
 ///
@@ -10,247 +12,47 @@ use crate::{DecodedKey, HandleControl, KeyCode, KeyboardLayout, Modifiers, Physi
 pub struct De105Key;
 
 impl KeyboardLayout for De105Key {
+    #[rustfmt::skip]
     fn map_keycode(
         &self,
         keycode: KeyCode,
         modifiers: &Modifiers,
         handle_ctrl: HandleControl,
     ) -> DecodedKey {
-        let map_to_unicode = handle_ctrl == HandleControl::MapLettersToUnicode;
         match keycode {
-            KeyCode::Escape => DecodedKey::Unicode('\u{001B}'),
-            KeyCode::Oem8 => {
-                if modifiers.is_shifted() {
-                    DecodedKey::Unicode('°')
-                } else {
-                    DecodedKey::Unicode('^')
-                }
-            }
-            KeyCode::Key1 => {
-                if modifiers.is_shifted() {
-                    DecodedKey::Unicode('!')
-                } else {
-                    DecodedKey::Unicode('1')
-                }
-            }
-            KeyCode::Key2 => {
-                if modifiers.is_altgr() {
-                    DecodedKey::Unicode('²')
-                } else if modifiers.is_shifted() {
-                    DecodedKey::Unicode('"')
-                } else {
-                    DecodedKey::Unicode('2')
-                }
-            }
-            KeyCode::Key3 => {
-                if modifiers.is_altgr() {
-                    DecodedKey::Unicode('³')
-                } else if modifiers.is_shifted() {
-                    DecodedKey::Unicode('§')
-                } else {
-                    DecodedKey::Unicode('3')
-                }
-            }
-            KeyCode::Key4 => {
-                if modifiers.is_shifted() {
-                    DecodedKey::Unicode('$')
-                } else {
-                    DecodedKey::Unicode('4')
-                }
-            }
-            KeyCode::Key5 => {
-                if modifiers.is_shifted() {
-                    DecodedKey::Unicode('%')
-                } else {
-                    DecodedKey::Unicode('5')
-                }
-            }
-            KeyCode::Key6 => {
-                if modifiers.is_shifted() {
-                    DecodedKey::Unicode('&')
-                } else {
-                    DecodedKey::Unicode('6')
-                }
-            }
-            KeyCode::Key7 => {
-                if modifiers.is_altgr() {
-                    DecodedKey::Unicode('{')
-                } else if modifiers.is_shifted() {
-                    DecodedKey::Unicode('/')
-                } else {
-                    DecodedKey::Unicode('7')
-                }
-            }
-            KeyCode::Key8 => {
-                if modifiers.is_altgr() {
-                    DecodedKey::Unicode('[')
-                } else if modifiers.is_shifted() {
-                    DecodedKey::Unicode('(')
-                } else {
-                    DecodedKey::Unicode('8')
-                }
-            }
-            KeyCode::Key9 => {
-                if modifiers.is_altgr() {
-                    DecodedKey::Unicode(']')
-                } else if modifiers.is_shifted() {
-                    DecodedKey::Unicode(')')
-                } else {
-                    DecodedKey::Unicode('9')
-                }
-            }
-            KeyCode::Key0 => {
-                if modifiers.is_altgr() {
-                    DecodedKey::Unicode('}')
-                } else if modifiers.is_shifted() {
-                    DecodedKey::Unicode('=')
-                } else {
-                    DecodedKey::Unicode('0')
-                }
-            }
-            KeyCode::OemMinus => {
-                if modifiers.is_altgr() {
-                    DecodedKey::Unicode('\\')
-                } else if modifiers.is_shifted() {
-                    DecodedKey::Unicode('?')
-                } else {
-                    DecodedKey::Unicode('ß')
-                }
-            }
-            KeyCode::OemPlus => {
-                if modifiers.is_shifted() {
-                    DecodedKey::Unicode('`')
-                } else {
-                    DecodedKey::Unicode('´')
-                }
-            }
-            KeyCode::Backspace => DecodedKey::Unicode('\u{0008}'),
-            KeyCode::Tab => DecodedKey::Unicode('\u{0009}'),
-            KeyCode::Q => {
-                if map_to_unicode && modifiers.is_ctrl() {
-                    DecodedKey::Unicode('\u{0011}')
-                } else if modifiers.is_altgr() {
-                    DecodedKey::Unicode('@')
-                } else if modifiers.is_caps() {
-                    DecodedKey::Unicode('Q')
-                } else {
-                    DecodedKey::Unicode('q')
-                }
-            }
-            KeyCode::E => {
-                if map_to_unicode && modifiers.is_ctrl() {
-                    DecodedKey::Unicode('\u{0005}')
-                } else if modifiers.is_altgr() {
-                    DecodedKey::Unicode('€')
-                } else if modifiers.is_caps() {
-                    DecodedKey::Unicode('E')
-                } else {
-                    DecodedKey::Unicode('e')
-                }
-            }
-            KeyCode::Y => {
-                if map_to_unicode && modifiers.is_ctrl() {
-                    DecodedKey::Unicode('\u{001A}')
-                } else if modifiers.is_caps() {
-                    DecodedKey::Unicode('Z')
-                } else {
-                    DecodedKey::Unicode('z')
-                }
-            }
-            KeyCode::Oem4 => {
-                if modifiers.is_caps() {
-                    DecodedKey::Unicode('Ü')
-                } else {
-                    DecodedKey::Unicode('ü')
-                }
-            }
-            KeyCode::Oem6 => {
-                if modifiers.is_altgr() {
-                    DecodedKey::Unicode('~')
-                } else if modifiers.is_caps() {
-                    DecodedKey::Unicode('*')
-                } else {
-                    DecodedKey::Unicode('+')
-                }
-            }
-            KeyCode::Return => DecodedKey::Unicode('\u{000A}'),
-            KeyCode::Oem7 => {
-                if modifiers.is_shifted() {
-                    DecodedKey::Unicode('\'')
-                } else {
-                    DecodedKey::Unicode('#')
-                }
-            }
-            KeyCode::Oem1 => {
-                if modifiers.is_shifted() {
-                    DecodedKey::Unicode('Ö')
-                } else {
-                    DecodedKey::Unicode('ö')
-                }
-            }
-            KeyCode::Oem3 => {
-                if modifiers.is_shifted() {
-                    DecodedKey::Unicode('Ä')
-                } else {
-                    DecodedKey::Unicode('ä')
-                }
-            }
-            KeyCode::Z => {
-                if map_to_unicode && modifiers.is_ctrl() {
-                    DecodedKey::Unicode('\u{0019}')
-                } else if modifiers.is_caps() {
-                    DecodedKey::Unicode('Y')
-                } else {
-                    DecodedKey::Unicode('y')
-                }
-            }
-            KeyCode::M => {
-                if map_to_unicode && modifiers.is_ctrl() {
-                    DecodedKey::Unicode('\u{000D}')
-                } else if modifiers.is_altgr() {
-                    DecodedKey::Unicode('µ')
-                } else if modifiers.is_caps() {
-                    DecodedKey::Unicode('M')
-                } else {
-                    DecodedKey::Unicode('m')
-                }
-            }
-            KeyCode::OemComma => {
-                if modifiers.is_shifted() {
-                    DecodedKey::Unicode(';')
-                } else {
-                    DecodedKey::Unicode(',')
-                }
-            }
-            KeyCode::OemPeriod => {
-                if modifiers.is_shifted() {
-                    DecodedKey::Unicode(':')
-                } else {
-                    DecodedKey::Unicode('.')
-                }
-            }
-            KeyCode::Oem2 => {
-                if modifiers.is_shifted() {
-                    DecodedKey::Unicode('_')
-                } else {
-                    DecodedKey::Unicode('-')
-                }
-            }
-            KeyCode::Oem5 => {
-                if modifiers.is_shifted() {
-                    DecodedKey::Unicode('>')
-                } else if modifiers.is_altgr() {
-                    DecodedKey::Unicode('|')
-                } else {
-                    DecodedKey::Unicode('<')
-                }
-            }
-            e => {
-                let us = super::Us104Key;
-                us.map_keycode(e, modifiers, handle_ctrl)
-            }
+            // ========= Row 2 (the numbers) =========
+            KeyCode::Oem8      => modifiers.handle_shift('^', '°'),
+            KeyCode::Key2      => modifiers.handle_altsh('2', '"', '²'),
+            KeyCode::Key3      => modifiers.handle_altsh('3', '§', '³'),
+            KeyCode::Key6      => modifiers.handle_shift('6', '&'),
+            KeyCode::Key7      => modifiers.handle_altsh('7', '/', '{'),
+            KeyCode::Key8      => modifiers.handle_altsh('8', '(', '['),
+            KeyCode::Key9      => modifiers.handle_altsh('9', ')', ']'),
+            KeyCode::Key0      => modifiers.handle_altsh('0', '=', '}'),
+            KeyCode::OemMinus  => modifiers.handle_altsh('ß', '?', SLS),
+            KeyCode::OemPlus   => modifiers.handle_shift('´', '`'),
+            // ========= Row 3 (QWERTY) =========
+            KeyCode::Q         => modifiers.handle_alalt('Q', '@', '@', handle_ctrl),
+            KeyCode::E         => modifiers.handle_alalt('E', '€', '€', handle_ctrl),
+            KeyCode::Y         => modifiers.handle_alpha('Z', handle_ctrl),
+            KeyCode::Oem4      => modifiers.handle_accen('ü', 'Ü'),
+            KeyCode::Oem6      => modifiers.handle_altsh('+', '*', '~'),
+            // ========= Row 4 (ASDFG) =========
+            KeyCode::Oem1      => modifiers.handle_accen('ö', 'Ö'),
+            KeyCode::Oem3      => modifiers.handle_accen('ä', 'Ä'),
+            KeyCode::Oem7      => modifiers.handle_shift('#', QUO),
+            // ========= Row 5 (ZXCVB) =========
+            KeyCode::Oem5      => modifiers.handle_altsh('<', '>', '|'),
+            KeyCode::Z         => modifiers.handle_alpha('Y', handle_ctrl),
+            KeyCode::M         => modifiers.handle_alalt('M', 'µ', 'µ', handle_ctrl),
+            KeyCode::OemComma  => modifiers.handle_shift(',', ';'),
+            KeyCode::OemPeriod => modifiers.handle_shift('.', ':'),
+            KeyCode::Oem2      => modifiers.handle_shift('-', '_'),
+            // ========= Fallback =========
+            e => super::Us104Key.map_keycode(e, modifiers, handle_ctrl),
         }
     }
+
     fn get_physical(&self) -> PhysicalKeyboard {
         PhysicalKeyboard::Iso
     }
