f4c00008845b660d60d748233404ffe873cb29cb
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2025-06-12 23:33:24 +0100

    Added USB HID Boot Keyboard support

--- src/scancodes/mod.rs
+++ src/scancodes/mod.rs
@@ -2,6 +2,8 @@
 
 mod set1;
 mod set2;
+mod usbhid;
 
 pub use self::set1::ScancodeSet1;
 pub use self::set2::ScancodeSet2;
+pub use self::usbhid::{convert as usb_convert, Modifiers as UsbModifiers};
